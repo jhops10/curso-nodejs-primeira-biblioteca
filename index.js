91dc98edae01320c485da132f5a91a5329b727ca
João Paulo <joaowais@gmail.com>
2023-07-29 23:50:48 -0300

    adicionado a leitura de arquivo fs e tratamento de erro

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
 import chalk from 'chalk';
 
-console.log(chalk.red('olá mundo'));
+function handleError(erro) {
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório.'));
+}
+
+function readFile(directory) {
+  const encoding = 'utf-8';
+  fs.readFile(directory, encoding, (erro, response) => {
+    if (erro) {
+      handleError(erro);
+    }
+    console.log(chalk.green(response))
+  });
+}
+
+readFile('./arquivos/texto.md');
