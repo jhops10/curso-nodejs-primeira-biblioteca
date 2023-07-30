02e155331170e3d5b3c323f3be1019860797e0ce
João Paulo <joaowais@gmail.com>
2023-07-30 19:13:44 -0300

    refatorando funcao openFile utilizando async await e then()

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+
+//Gerenciar Erros
 function handleError(erro) {
   throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório.'));
 }
 
-function readFile(directory) {
-  const encoding = 'utf-8';
-  fs.readFile(directory, encoding, (erro, response) => {
-    if (erro) {
-      handleError(erro);
-    }
-    console.log(chalk.green(response))
-  });
+//Abrir o Aqruivo
+
+// Utilizando o método then()
+
+// function openFile(directory) {
+//   const encoding = 'utf-8';
+//   fs.readFile(directory, encoding, (erro, response) => {
+//     if (erro) {
+//       handleError(erro);
+//     }
+//     console.log(chalk.green(response))
+//   });
+// }
+
+
+//Utilizando o método async await
+
+async function openFile(directory) {
+  try {
+    const encoding = 'utf-8';
+    const response = await fs.promises.readFile(directory, encoding);
+    console.log(chalk.green(response));
+  } catch (erro) {
+    handleError(erro);
+  }
 }
 
-readFile('./arquivos/texto.md');
+openFile('./arquivos/texto.md');
