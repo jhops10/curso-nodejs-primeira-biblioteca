07b2b17371549273fd7bb4224e1ca4fb8b7aeaba
João Paulo <joaowais@gmail.com>
2023-07-31 21:09:55 -0300

    criada funcao extractLinks que retorna lista validada de links

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,7 @@
+function extractLinks(arrLinks) {
+  return arrLinks.map((objLink) => Object.values(objLink).join())
+}
+
 export default function validatedList(linksList) {
-  return 'Entrou na função'
+  return extractLinks(linksList);
 }
